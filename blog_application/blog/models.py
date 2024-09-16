64766a6055809fd6e6b42a4789e0fb8503b4ca74
Soheil <iamsoheil.r@gmail.com>
2024-09-16 14:31:47 +0330

    Add status field

--- blog_application/blog/models.py
+++ blog_application/blog/models.py
@@ -6,7 +6,14 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
+    class Status(models.TextChoices):
+        # Enumeration class
+        # <choice_name/member> = <value>, <readable_name/label>
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
     title = models.CharField(max_length=250)
+    # Slug --> letters + numbers + underscores + hyphens
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -14,9 +21,12 @@ class Post(models.Model):
     # publish = models.DateTimeField(db_default=Now())
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
 
     class Meta:
         ordering = ["-publish"]
+        # Hyphen is used to define descending order
+        # ordering will apply by default for all queries unless an order is mentioned
         indexes = [models.Index(fields=["-publish"])]
 
     def __str__(self):
